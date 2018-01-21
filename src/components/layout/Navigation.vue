b74fc2a872116161d275091009082f2fca732ee2
Matteo Hertel <info@matteohertel.com>
2018-01-21 17:09:49 +0100

    add navigation component

---
+++ src/components/layout/Navigation.vue
@@ -0,0 +1,20 @@
+<template functional>
+<v-list dense>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>dashboard</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Dashboard</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Settings</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+</v-list>
+</template>
