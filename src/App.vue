30985658fc156017c58b755c4f9834eabdfa02a4
Matteo Hertel <info@matteohertel.com>
2018-02-17 22:35:07 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+<SortingVis />
+<SortingVis />
+<SortingVis />
+<SortingVis />
+<SortingVis />
+<SortingVis />
+<SortingVis />
+<SortingVis />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import SortingVis from "./components/SortingVis.vue";
+
+export default {
+  name: "app",
+  components: {
+    HelloWorld,
+    SortingVis
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
