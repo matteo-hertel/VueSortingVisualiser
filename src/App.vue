cfce081f259490480b6a6823372018d76c7a5ddd
Matteo Hertel <info@matteohertel.com>
2018-02-18 22:03:00 +0000

    Passing snapshots as props

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <SortingVis />
+    <SortingVis :snapshots="snapshots"/>
   </div>
 </template>
 
@@ -11,11 +11,73 @@ import SortingVis from "./components/SortingVis.vue";
 
 export default {
   name: "app",
+  data: () => getData(tmp),
   components: {
     HelloWorld,
     SortingVis
   }
 };
+const snapshots = [];
+const snapshot = array => snapshots.push(Array.from(array));
+const cocktailSort = arr => {
+  function swap(arr, a, b) {
+    snapshot(arr);
+    if (a === b) return arr;
+
+    var temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+    return arr;
+  }
+
+  var i,
+    left = 0,
+    right = arr.length - 1;
+
+  while (left < right) {
+    for (i = left; i < right; i++)
+      if (arr[i] > arr[i + 1]) {
+        arr = swap(arr, i, i + 1);
+      }
+
+    right--;
+    for (i = right; i > left; i--)
+      if (arr[i - 1] > arr[i]) {
+        arr = swap(arr, i - 1, i);
+      }
+
+    left++;
+  }
+  return arr;
+};
+function getData(arr) {
+  cocktailSort(arr);
+  return {
+    snapshots: snapshots
+  };
+}
+const tmp = [
+  7,
+  14,
+  4,
+  15,
+  8,
+  13,
+  6,
+  3,
+  1,
+  18,
+  2,
+  5,
+  10,
+  16,
+  12,
+  17,
+  19,
+  11,
+  9,
+  20
+];
 </script>
 
 <style>
--- src/components/SortingVis.vue
+++ src/components/SortingVis.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="hello">
-  <div class="grid-container" v-html="getCurrent"></div>
-  <button @click="startProcess">Get Next</button>
- </div>
+	<div class="grid-container" v-html="getCurrent"></div> 
+</div>
 </template>
 
 <script>
@@ -10,13 +9,12 @@ export default {
   name: "SortingVis",
   data: getData,
   methods: {
-   getPixel(colour){
-return `<div class="item" style="height:25px;background-color:${colour}"></div>`;
-},
- startProcess() {
+    getPixel(colour) {
+      return `<div class="item" style="height:20px;background-color:${colour}"></div>`;
+    },
+    startProcess() {
       setInterval(() => {
-console.log(this.currentArray , this.rawArray.length);
-        if (this.currentArray < this.rawArray.length) {
+        if (this.currentArray < this.snapshots.length) {
           this.currentArray++;
         }
       }, 300);
@@ -24,352 +22,31 @@ console.log(this.currentArray , this.rawArray.length);
   },
   computed: {
     getCurrent() {
-      return this.rawArray[this.currentArray].map(getColor).map(this.getPixel).join("");
+      return this.snapshots[this.currentArray]
+        .map(getColor)
+        .map(this.getPixel)
+        .join("");
     }
   },
-  props: {},
-}
+  props: {
+    snapshots: Array
+  }
+};
 function getColor(input) {
-  const max = 25;
-  const value = 25 - input;
+  const max = 20;
+  const value = 20 - input;
 
   const spectrum = value / max;
   const red =
-    spectrum < 0.5 ? Math.floor(Math.abs(spectrum - 0.5) * 2 * 255) : 0;
-  const blue = spectrum > 0.5 ? Math.floor((spectrum - 0.5) * 2 * 255) : 0;
-  const green = blue > 0 ? 255 - blue : 255 - red;
+    spectrum < 0.5 ? Math.floor(Math.abs(spectrum - 0.5) * 2 * 205) : 0;
+  const blue = spectrum > 0.5 ? Math.floor((spectrum - 0.5) * 2 * 205) : 0;
+  const green = blue > 0 ? 205 - blue : 205 - red;
 
   return `rgb(${red},${green},${blue})`;
 }
 function getData() {
   return {
-    currentArray: 0,
-    rawArray: [
-      [
-        1,
-        8,
-        18,
-        23,
-        16,
-        13,
-        20,
-        21,
-        9,
-        5,
-        6,
-        7,
-        17,
-        4,
-        25,
-        15,
-        3,
-        22,
-        11,
-        10,
-        24,
-        2,
-        12,
-        19,
-        14
-      ],
-      [
-        1,
-        8,
-        18,
-        16,
-        23,
-        13,
-        20,
-        21,
-        9,
-        5,
-        6,
-        7,
-        17,
-        4,
-        25,
-        15,
-        3,
-        22,
-        11,
-        10,
-        24,
-        2,
-        12,
-        19,
-        14
-      ],
-      [
-        1,
-        8,
-        18,
-        16,
-        13,
-        23,
-        20,
-        21,
-        9,
-        5,
-        6,
-        7,
-        17,
-        4,
-        25,
-        15,
-        3,
-        22,
-        11,
-        10,
-        24,
-        2,
-        12,
-        19,
-        14
-      ],
-      [
-        1,
-        8,
-        18,
-        16,
-        13,
-        20,
-        23,
-        21,
-        9,
-        5,
-        6,
-        7,
-        17,
-        4,
-        25,
-        15,
-        3,
-        22,
-        11,
-        10,
-        24,
-        2,
-        12,
-        19,
-        14
-      ],
-      [
-        1,
-        8,
-        18,
-        16,
-        13,
-        20,
-        21,
-        23,
-        9,
-        5,
-        6,
-        7,
-        17,
-        4,
-        25,
-        15,
-        3,
-        22,
-        11,
-        10,
-        24,
-        2,
-        12,
-        19,
-        14
-      ],
-      [
-        1,
-        8,
-        18,
-        16,
-        13,
-        20,
-        21,
-        9,
-        23,
-        5,
-        6,
-        7,
-        17,
-        4,
-        25,
-        15,
-        3,
-        22,
-        11,
-        10,
-        24,
-        2,
-        12,
-        19,
-        14
-      ],
-      [
-        1,
-        8,
-        18,
-        16,
-        13,
-        20,
-        21,
-        9,
-        5,
-        23,
-        6,
-        7,
-        17,
-        4,
-        25,
-        15,
-        3,
-        22,
-        11,
-        10,
-        24,
-        2,
-        12,
-        19,
-        14
-      ],
-      [
-        1,
-        8,
-        18,
-        16,
-        13,
-        20,
-        21,
-        9,
-        5,
-        6,
-        23,
-        7,
-        17,
-        4,
-        25,
-        15,
-        3,
-        22,
-        11,
-        10,
-        24,
-        2,
-        12,
-        19,
-        14
-      ],
-      [
-        1,
-        8,
-        18,
-        16,
-        13,
-        20,
-        21,
-        9,
-        5,
-        6,
-        7,
-        23,
-        17,
-        4,
-        25,
-        15,
-        3,
-        22,
-        11,
-        10,
-        24,
-        2,
-        12,
-        19,
-        14
-      ],
-      [
-        1,
-        8,
-        18,
-        16,
-        13,
-        20,
-        21,
-        9,
-        5,
-        6,
-        7,
-        17,
-        23,
-        4,
-        25,
-        15,
-        3,
-        22,
-        11,
-        10,
-        24,
-        2,
-        12,
-        19,
-        14
-      ],
-      [
-        1,
-        8,
-        18,
-        16,
-        13,
-        20,
-        21,
-        9,
-        5,
-        6,
-        7,
-        17,
-        4,
-        23,
-        25,
-        15,
-        3,
-        22,
-        11,
-        10,
-        24,
-        2,
-        12,
-        19,
-        14
-      ],
-      [
-        1,
-        8,
-        18,
-        16,
-        13,
-        20,
-        21,
-        9,
-        5,
-        6,
-        7,
-        17,
-        4,
-        23,
-        15,
-        25,
-        3,
-        22,
-        11,
-        10,
-        24,
-        2,
-        12,
-        19,
-        14
-      ]
-    ]
+    currentArray: 0
   };
 }
 </script>
@@ -391,7 +68,7 @@ a {
   color: #42b983;
 }
 .grid-container {
-  --grid-count: 25
+  --grid-count: 20;
 }
 .grid-container {
   width: 550px;
@@ -402,9 +79,8 @@ a {
 }
 
 .item {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
